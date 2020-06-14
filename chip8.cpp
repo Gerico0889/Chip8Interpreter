cf9eb939d756119035267fdee442fd481b52e301
Gerico0889 <gerico0889@gmail.com>
2020-06-14 18:40:24 +0000

    Added some instructions

--- chip8.cpp
+++ chip8.cpp
@@ -66,6 +66,132 @@ struct chip8 {
 		PC = opcode & 0xFFFu;
 	}
 
+	// SE Vx, byte: if Vx == kk increase the PC by 2
+	void OP_3xkk() {
+		// Vx indicates the 4 least significant bits of the high byte
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+
+		// kk indicates the lowest 8bits of the instruction
+		const unsigned kk = opcode & 0x00FF;
+
+		if (Reg[Vx] == kk) PC += 2;
+	}
+
+	// SNE Vx, byte: if Vx != kk increase the PC by 2
+	void OP_4xkk() {
+		// Vx indicates the 4 least significant bits of the high byte
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+
+		// kk indicates the lowest 8bits of the instruction
+		const unsigned kk = opcode & 0x00FF;
+
+		if (Reg[Vx] != kk) PC += 2;
+	}
+
+	// SE Vx, Vy: if Vx == Vy increase the PC by 2
+	void OP_5xy0() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+
+		// Vy indicates the 4 most significant bits of the low byte
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		if (Reg[Vx] == Reg[Vy]) PC += 2;
+	}
+
+	// LD Vx, byte: set Vx = kk
+	void OP_6xkk() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned kk = opcode & 0x00FF;
+
+		Reg[Vx] = kk;
+	}
+
+	// ADD Vx, byte: set Vx = Vx + kk
+	void OP_7xkk() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned kk = opcode & 0x00FF;
+
+		Reg[Vx] += kk;
+	}
+
+	// LD Vx, Vy: set Vx = Vy
+	void OP_8xy0() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		Reg[Vx] = Reg[Vy];
+	}
+
+	// OR Vx, Vy: set Vx = Vx or Vy
+	void OP_8xy1() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		Reg[Vx] |= Reg[Vy];
+	}
+
+	// AND Vx, Vy: set Vx = Vx and Vy
+	void OP_8xy2() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		Reg[Vx] &= Reg[Vy];
+	}
+
+	// XOR Vx, Vy: set Vx = Vx xor Vy
+	void OP_8xy3() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		Reg[Vx] ^= Reg[Vy];
+	}
+
+	// ADD Vx, Vy: set Vx = Vx + Vy, set VF = carry
+	void OP_8xy4() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		unsigned res = Reg[Vx] + Reg[Vy];
+		Reg[0xF] = res >> 8u;
+		Reg[Vx] = res & 0x00FFu;
+	}
+
+	// SUB Vx, Vy: set Vx = Vx - Vy, set VF = NOT borrow
+	void OP_8xy5() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		unsigned res = Reg[Vx] - Reg[Vy];
+		Reg[0xF] = !(res >> 8u);
+		Reg[Vx] = res;
+	}
+
+	// SHR Vx {, Vy}: set Vx = Vx SHR 1
+	void OP_8xy6() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+
+		Reg[0xF] = Reg[Vx] & 0x1u;
+		Reg[Vx] >>= 1;
+	}
+
+	// SUBN Vx, Vy: set Vx = Vy - Vx, VF = NOT borrow
+	void OP_8xy7() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+		const unsigned Vy = (opcode & 0x00F0) >> 4u;
+
+		unsigned res = Reg[Vy] - Reg[Vx];
+		Reg[0xF] = !(res >> 8u);
+		Reg[Vx] = res;
+	}
+
+	// SHL Vx {, Vy}: set Vx = Vx SHL 1
+	void OP_8xyE() {
+		const unsigned Vx = (opcode & 0x0F00) >> 8u;
+
+		Reg[0xF] = (Reg[Vx] & 0x80u) >> 7u;
+		Reg[Vx] <<= 1;
+	}
+
 };
 
 
